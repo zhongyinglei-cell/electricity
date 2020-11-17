8031a66bb3ec72bdee8e98adc69c469fe1b00114
钟莹磊 <zhongyinglei0212@163.com>
2020-11-17 09:45:54 +0800

    格式整理

--- prjectApp/urls.py
+++ prjectApp/urls.py
@@ -2,14 +2,14 @@ from django.conf.urls import url
 from prjectApp import views
 
 urlpatterns = [
-    url(r'^login/',views.login,name="login"),
-    url(r'^home/',views.home,name='home'),
-    url(r'^user/',views.user,name="user"),
-    url(r'^$',views.index,name='index'),
-    url(r'^manage/',views.manage,name='manage'),
-    url(r'^error/',views.error,name='error'),
-    url(r'^logout/',views.logout,name='logout'),
-    url(r'^choujiang/',views.choujiang,name='choujiang'),
+    url(r'^login/', views.login, name="login"),
+    url(r'^home/', views.home, name='home'),
+    url(r'^user/', views.user, name="user"),
+    url(r'^$', views.index, name='index'),
+    url(r'^manage/', views.manage, name='manage'),
+    url(r'^error/', views.error, name='error'),
+    url(r'^logout/', views.logout, name='logout'),
+    url(r'^choujiang/', views.choujiang, name='choujiang'),
     url(r'^chongzhi/', views.chongzhi, name='chongzhi'),
     url(r'^zhuce/', views.zhuce, name='zhuce'),
 ]
